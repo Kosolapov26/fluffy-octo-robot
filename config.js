1fba552568f9337a05a7b5366da581301441ee20
Kosolapov26 <153091839+Kosolapov26@users.noreply.github.com>
2026-05-16 23:08:28 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// ========== ГЛОБАЛЬНАЯ КОНФИГУРАЦИЯ ==========
+const SITE_CONFIG = {
+    PLAYER_TOKEN: 'c9fe4f828ec190451e8bb5f66659838d',
+    PLAYER_DOMAIN: 'https://tv-2-kinoserial.net'
+};
+
+function getPlayerUrl(kpId, imdbId, tmdbId, type) {
+    let idValue = '';
+    switch(type) {
+        case 'imdb': idValue = imdbId; break;
+        case 'tmdb': idValue = `tmdb${tmdbId}`; break;
+        default: idValue = imdbId;
+    }
+    return `${SITE_CONFIG.PLAYER_DOMAIN}/embed_auto/${idValue}/?token=${SITE_CONFIG.PLAYER_TOKEN}`;
+}
